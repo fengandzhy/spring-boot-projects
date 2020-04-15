647e8c7caaf20b397e856d08b90514fe3bcc24d0
unknown <fengandzhy@gmail.com>
2020-04-16 03:45:00 +0800

    add an repository interface

---
+++ spring-boot-project1/src/main/java/org/zhouhy/springboot/project1/repository/CompanyRepository.java
@@ -0,0 +1,25 @@
+package org.zhouhy.springboot.project1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.zhouhy.springboot.project1.domain.Company;
+
+import java.util.List;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,String> {
+
+    //原生sql语句查询
+    @Query(value="select * from t_company where company_name=?1",nativeQuery = true)
+    List<Company> findByNativeSQL(String companyName);
+
+    @Query(value="select * from t_company where company_name like '%?1%'",nativeQuery = true)
+    List<Company> findByNativeSQL1(String companyName);
+
+    //原生sql语句操作（涉及到数据变动的，如删除和更新，必须加注解@Modifying）
+    @Modifying
+    @Query(value = "update t_company set company_address =?1 where company_name=?2",nativeQuery = true)
+    void updateByName(String companyAddress,String companyName);
+}
